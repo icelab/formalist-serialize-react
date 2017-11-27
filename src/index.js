9621861cf0e8d6f18abeee5ace41f83071394bdc
Max Wheeler <max@makenosound.com>
2017-11-27 15:22:09 +1100

    Run through prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import createDataObjectRenderer from 'formalist-data-object-renderer'
-import { List, Map } from 'immutable'
+import React from "react";
+import createDataObjectRenderer from "formalist-data-object-renderer";
+import { List, Map } from "immutable";
 
 /**
  * Simple check to see if a variable is an object
@@ -9,7 +9,7 @@ import { List, Map } from 'immutable'
  */
 
 function isObject(obj) {
-  return obj === Object(obj)
+  return obj === Object(obj);
 }
 
 /**
@@ -19,67 +19,66 @@ function isObject(obj) {
  * @return {String} name : "foo[bar][baz]"
  */
 
-function serializeName (path) {
-  return path.map((s, i) => {
-    return i === 0 ? s : '[' + s + ']'
-  }).join('')
+function serializeName(path) {
+  return path
+    .map((s, i) => {
+      return i === 0 ? s : "[" + s + "]";
+    })
+    .join("");
 }
 
-function render (name, value, path = []) {
+function render(name, value, path = []) {
   if (value && value.toJS) {
-    value = value.toJS()
+    value = value.toJS();
   }
   if (Array.isArray(value)) {
-    return renderList(name, value, path)
+    return renderList(name, value, path);
   } else if (isObject(value)) {
-    return renderMap(name, value, path)
+    return renderMap(name, value, path);
   }
-  return renderValue(name, value, path)
+  return renderValue(name, value, path);
 }
 
-function renderValue (name, value, path) {
-  const append = Array.isArray(name) ? name : [name]
-  path = path.concat(append)
-  name = serializeName(path)
-  value =  (value != null) ? value : ''
-  const key = [name, value]
-  return React.createElement(
-    'input',
-    {
-      key,
-      name,
-      value,
-      type: 'hidden',
-    }
-  )
+function renderValue(name, value, path) {
+  const append = Array.isArray(name) ? name : [name];
+  path = path.concat(append);
+  name = serializeName(path);
+  value = value != null ? value : "";
+  const key = [name, value];
+  return React.createElement("input", {
+    key,
+    name,
+    value,
+    type: "hidden"
+  });
 }
 
-function renderList (name, value, path) {
+function renderList(name, value, path) {
   if (value.length > 0) {
-    return value.map((c) => {
+    return value.map(c => {
       // Insert a junk value as the _first_ item in an array where the contents of
       // the array are a map to ensure params are parsed correctly by rack
       // https://github.com/rack/rack/issues/951
       if (isObject(c) || Map.isMap(c)) {
         return [
-          render([name, '', '__rack_workaround'], '', path),
-          render([name, ''], c, path)
-        ]
+          render([name, "", "__rack_workaround"], "", path),
+          render([name, ""], c, path)
+        ];
       } else {
-        return render([name, ''], c, path);
+        return render([name, ""], c, path);
       }
-    })
+    });
   } else {
-    return render(name, '', path);
+    return render(name, "", path);
   }
 }
 
-function renderMap (name, value, path) {
-  const append = Array.isArray(name) ? name : [name]
-  path = path.concat(append)
+function renderMap(name, value, path) {
+  const append = Array.isArray(name) ? name : [name];
+  path = path.concat(append);
   return Object.keys(value).map((key, index) => {
-    return render(key, value[key], path)
-  })
+    return render(key, value[key], path);
+  });
 }
 
 /**
@@ -89,12 +88,12 @@ function renderMap (name, value, path) {
  * @return {Array} An array of React elements
  */
 
-export default function serialize (ast, options = {}) {
-  const { prefix } = options
-  const path = (prefix) ? [prefix] : []
-  const dataObjectRenderer = createDataObjectRenderer()
-  const dataObject = dataObjectRenderer(ast)
+export default function serialize(ast, options = {}) {
+  const { prefix } = options;
+  const path = prefix ? [prefix] : [];
+  const dataObjectRenderer = createDataObjectRenderer();
+  const dataObject = dataObjectRenderer(ast);
   return Object.keys(dataObject).map((key, index) => {
-    return render(key, dataObject[key], path)
-  })
+    return render(key, dataObject[key], path);
+  });
 }
